821932cb373fcbe4c8333345371f5e9a6a80d891
candaceleach41 <candacemleach41@gmail.com>
2021-05-31 19:49:49 -0400

    initial commit

---
+++ valid_palindrome_ii.py
@@ -0,0 +1,41 @@
+"""
+Given a string s, return true if the s can be palindrome after deleting at most one
+character from it.
+
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+"""
+
+
+# Time: O(n)
+# Space: O(1)
+def valid_palindrome(s):
+    def verify(s, left, right, deleted):
+        while left < right:
+            if s[left] != s[right]:
+                if deleted:
+                    return False
+                else:
+                    return verify(s, left + 1, right, True) or verify(s, left, right - 1, True)
+            else:
+                left += 1
+                right -= 1
+        return True
+
+    return verify(s, 0, len(s) - 1, False)
