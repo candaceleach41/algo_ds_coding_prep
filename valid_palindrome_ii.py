e28559cbeffb55ec09a7573fc85d1626ab66accf
candace leach <candacemleach41@gmail.com>
2021-06-18 10:50:25 -0400

    added another solution

--- valid_palindrome_ii.py
+++ valid_palindrome_ii.py
@@ -39,3 +39,29 @@ def valid_palindrome(s):
         return True
 
     return verify(s, 0, len(s) - 1, False)
+
+
+# Another Solution ---------------------------------------------------------------------------
+# Time: O(n)
+# Space: O(1)
+def valid_palindrome_v2(s):
+    low = 0
+    high = len(s) - 1
+    while low < high:
+        if s[low] != s[high]:
+            return is_palindrome(s, low + 1, high) or is_palindrome(s, low, high - 1)
+        low += 1
+        high -= 1
+
+
+def is_palindrome(string, low, high):
+    while low < high:
+        if string[low] != string[high]:
+            return False
+        low += 1
+        high -= 1
+    return True
+
+
+if __name__ == "__main__":
+    print(valid_palindrome_v2("abcba"))
