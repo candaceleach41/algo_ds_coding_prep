c1aaabb8a7223eca0f74212768eec8859499f56c
candace leach <candacemleach41@gmail.com>
2021-06-22 12:29:43 -0400

    added another solution

--- squares_of_a_sorted_array.py
+++ squares_of_a_sorted_array.py
@@ -16,10 +16,29 @@ Output: [4,9,9,49,121]
 
 # Time: O(n log n) - because of sorting
 # Space: O(1) - no additional space used
-def sorted_squares(self, nums):
+def sorted_squares(nums):
     for i in range(len(nums)):
         nums[i] *= nums[i]
     # nums.sort()
     # return nums
     return sorted(nums)  # Added after comparing my solution to an optimal one
 
+
+# ----------------------------------------------
+# Another Solution (using two pointers)
+# Time: O(n)
+# Space: O(n)
+def sqrt_sorted_arr(nums):
+    n = len(nums)
+    result = [0] * n
+    left = 0
+    right = n - 1
+    for i in range(n - 1, -1, -1):
+        if abs(nums[left]) < abs(nums[right]):
+            square = nums[right]
+            right -= 1
+        else:
+            square = nums[left]
+            left += 1
+        result[i] = square * square
+    return result
