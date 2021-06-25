587f959f8e3cae8119b96c519998457d9dfa8722
candace leach <candacemleach41@gmail.com>
2021-06-25 14:36:52 -0400

    initial commit

---
+++ majority_element.py
@@ -0,0 +1,26 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+"""
+
+
+def majority_elem(nums):
+    candidate, count = nums[0], 0
+    for num in nums:
+        if num == candidate:
+            count += 1
+        elif count == 0:
+            candidate, count = num, 1
+        else:
+            count -= 1
+    return candidate
