fd5f26a3832d27ee0b3d9470c12121769a2cae08
candace leach <candacemleach41@gmail.com>
2021-10-24 16:05:35 -0400

    added a solution

--- group_anagrams.py
+++ group_anagrams.py
@@ -33,9 +33,24 @@ def group_anagrams(string):
 
 # using defaultdict
 from collections import defaultdict
+
+
 def group_anagrams_2(string):
     word = defaultdict(list)
     for st in string:
         s = ''.join(sorted(s))
         word[s].append(st)
     return word.values()
+
+
+# Not sorting characters
+def group_anagrams_3(string):
+    result = defaultdict(list)
+    for s in string:
+        count = [0] * 26  # a ... z since all chars are lowercase
+
+        for ch in s:
+            count[ord(ch) - ord("a")] += 1
+        result[tuple(count)].append(s)  # a list cannot be a key so turn it into a tuple
+
+    return result.values()
